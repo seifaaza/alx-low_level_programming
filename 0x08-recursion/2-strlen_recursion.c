add05d13d7c5085a45cbec65f9dd69616ae8b40b
seifeddine <seifeddine.aaza@gmail.com>
2023-09-28 09:37:45 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion : returns the length of a string
+ * _strlen_recursion _ returns the length of a string
  * @s: return string
  * Return: strlen_recursion
  */
